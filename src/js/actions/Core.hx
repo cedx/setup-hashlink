0a0aa3e614f037eee95a8ee603e0a74ceffd5924
Cédric Belin <cedric@belin.io>
2021-11-02 21:55:50 +0100

    Added externs for the NPM dependencies

---
+++ src/js/actions/Core.hx
@@ -0,0 +1,25 @@
+package js.actions;
+
+import haxe.extern.EitherType;
+import js.lib.Error;
+
+/** Functions for setting results, logging, registering secrets and exporting variables across actions. **/
+@:jsRequire("@actions/core")
+extern class Core {
+
+	/** Prepends the given input `path` to the jobs `PATH`. **/
+	static function addPath(path: String): Void;
+
+	/** Gets the value of an input. **/
+	static function getInput(name: String, ?options: InputOptions): String;
+
+	/** Logs the specified `message` and sets the action status to failed. **/
+	static function setFailed(message: EitherType<Error, String>): Void;
+}
+
+/** Defines the options of an action input. **/
+typedef InputOptions = {
+
+	/** Value indicating whether the input is required. **/
+	var required: Bool;
+}
