b7f0420d8c92ea2953434523b20011123523cf7e
Cédric Belin <cedric@belin.io>
2021-11-02 21:48:38 +0100

    Updated the build system

--- scripts/Dist.hx
+++ scripts/Dist.hx
@@ -1,4 +1,15 @@
+import Sys.*;
+import sys.FileSystem.*;
+import sys.io.File.*;
+
 /** Runs the script. **/
 function main() {
-	// TODO
+	for (script in ["Clean", "Version"]) command('lix $script');
+	command("haxe build.hxml");
+
+	final file = "bin/setup_hashlink.js";
+	if (exists('$file.map')) deleteFile('$file.map');
+	command('npx ncc build $file --minify --out=bin');
+	saveContent(file, '#!/usr/bin/env node\n${getContent(file)}');
+	if (systemName() != "Windows") command('chmod +x $file');
 }
