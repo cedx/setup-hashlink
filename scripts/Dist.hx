724cec7cbb669960dd3a0105121a74ef56afbbcd
Cédric Belin <cedric@belin.io>
2022-04-16 18:47:26 +0200

    Updated the build system

--- scripts/Dist.hx
+++ scripts/Dist.hx
@@ -1,4 +1,4 @@
-import sys.io.File;
+import sys.FileSystem;
 
 /** Runs the script. **/
 function main() {
@@ -6,8 +6,8 @@ function main() {
 	Sys.command("haxe --dce full build.hxml");
 
 	final file = "bin/setup_hashlink.js";
-	Sys.command('npx ncc build $file --minify --out=var');
-	File.saveContent(file, '#!/usr/bin/env node\n${File.getContent("var/index.js")}');
+	Sys.command('npx ncc build $file --minify --out=var --target=es2020');
+	FileSystem.rename("var/index.js", file);
 	Sys.command('git update-index --chmod=+x $file');
 	if (Sys.systemName() != "Windows") Sys.command('chmod +x $file');
 }
