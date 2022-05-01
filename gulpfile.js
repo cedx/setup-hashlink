0c1b3f50fc592225ddb5c4b0b6d823d9c84fbe04
Cédric Belin <cedric@belin.io>
2022-05-01 02:06:54 +0200

    Updated the build system [skip ci]

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,19 @@
-import {spawn} from "node:child_process";
 import {readFileSync} from "node:fs";
 import {cp, readFile, writeFile} from "node:fs/promises";
 import {EOL} from "node:os";
 import del from "del";
+import {execa} from "execa";
 import gulp from "gulp";
 import replace from "gulp-replace";
 
 // The package configuration.
 const pkg = JSON.parse(readFileSync("package.json", "utf8"));
 
-/** The default task. */
-export default gulp.series(
-	clean,
-	build,
-	version
-);
-
 /** Builds the project. */
 export async function build() {
-	await exec("npx", ["ncc", "build", "lib/main.js", "--minify", "--out=var", "--target=es2022"]);
+	await exec("ncc", ["build", "lib/main.js", "--minify", "--out=var", "--target=es2022"]);
 	await writeFile("bin/setup_hashlink.mjs", `#!/usr/bin/env node${EOL}${await readFile("var/index.js", "utf8")}`);
-	return exec("npx", ["tsc", "--project", "lib/jsconfig.json"]);
+	return exec("tsc", ["--project", "lib/jsconfig.json"]);
 }
 
 /** Deletes all generated files and reset any saved state. */
@@ -30,15 +23,15 @@ export function clean() {
 
 /** Builds the documentation. */
 export async function doc() {
-	await exec("npx", ["typedoc", "--options", "etc/typedoc.json"]);
+	await exec("typedoc", ["--options", "etc/typedoc.json"]);
 	return cp("www/favicon.ico", "docs/favicon.ico");
 }
 
 /** Performs the static analysis of source code. */
 export async function lint() {
 	const sources = JSON.parse(await readFile("jsconfig.json", "utf8")).include;
-	await exec("npx", ["eslint", "--config=etc/eslint.json", ...sources]);
-	return exec("npx", ["tsc", "--project", "jsconfig.json"]);
+	await exec("eslint", ["--config=etc/eslint.json", ...sources]);
+	return exec("tsc", ["--project", "jsconfig.json"]);
 }
 
 /** Publishes the package in the registry. */
@@ -48,7 +41,7 @@ export async function publish() {
 
 /** Runs the test suite. */
 export function test() {
-	return exec("npx", ["c8", "--all", "--include=lib/**/*.js", "--report-dir=var", "--reporter=lcovonly", "node_modules/.bin/mocha", "--recursive"]);
+	return exec("c8", ["--all", "--include=lib/**/*.js", "--report-dir=var", "--reporter=lcovonly", "node_modules/.bin/mocha", "--recursive"]);
 }
 
 /** Updates the version number in the sources. */
@@ -58,16 +51,23 @@ export function version() {
 
 /** Watches for file changes. */
 export function watch() {
-	return exec("npx", ["tsc", "--project", "jsconfig.json", "--watch"]);
+	return exec("tsc", ["--project", "jsconfig.json", "--watch"]);
 }
 
+/** Runs the default task. */
+export default gulp.series(
+	clean,
+	build,
+	version
+);
+
 /**
- * Spawns a new process using the specified command.
+ * Runs the specified command.
  * @param {string} command The command to run.
  * @param {string[]} [args] The command arguments.
- * @return {Promise<void>} Resolves when the command is finally terminated.
+ * @param {import("execa").Options} [options] The child process options.
+ * @returns {import("execa").ExecaChildProcess} Resolves when the command is finally terminated.
  */
-function exec(command, args = []) {
-	return new Promise((resolve, reject) => spawn(command, args, {shell: true, stdio: "inherit"})
-		.on("close", code => code ? reject(new Error(args.length ? `${command} ${args.join(" ")}` : command)) : resolve()));
+function exec(command, args = [], options = {}) {
+	return execa(command, args, {preferLocal: true, stdio: "inherit", ...options});
 }
