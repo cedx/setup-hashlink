052d7c393bb97338560b6ed7a8d8ec00f49c40de
Cédric Belin <cedric@belin.io>
2022-07-15 12:50:20 +0200

    Using the new describe/it API

--- test/release_test.js
+++ test/release_test.js
@@ -1,75 +1,78 @@
 import assert from "node:assert/strict";
-import test from "node:test";
+// @ts-expect-error TS2614
+import {describe, it} from "node:test";
 import {Platform, Release} from "../src/index.js";
 
-// A release that exists.
-const existingRelease = new Release({version: "1.0.0", assets: [
-	{platform: Platform.linux, file: "hl-1.0.zip"},
-	{platform: Platform.macOS, file: "hl-1.0.zip"},
-	{platform: Platform.windows, file: "hl-1.0.zip"}
-]});
+/**
+ * Tests the features of the {@link Release} class.
+ */
+describe("Release", () => {
+	const nonExistentRelease = new Release({version: "666.6.6"});
+	const existingRelease = new Release({version: "1.0.0", assets: [
+		{platform: Platform.linux, file: "hl-1.0.zip"},
+		{platform: Platform.macOS, file: "hl-1.0.zip"},
+		{platform: Platform.windows, file: "hl-1.0.zip"}
+	]});
 
-// A release that doesn't exist.
-const nonExistentRelease = new Release({version: "666.6.6"});
-
-test("Release.exists", async ctx => {
-	await ctx.test("should return `false` if the release does not exist", () => assert.ok(!nonExistentRelease.exists));
-	await ctx.test("should return `true` if the release exists", () => assert.ok(existingRelease.exists));
-});
-
-test("Release.isSource", async ctx => {
-	await ctx.test("should return `false` if the release is provided as binary", () => assert.ok(!existingRelease.isSource));
-	await ctx.test("should return `true` if the release is provided as source code", () => assert.ok(nonExistentRelease.isSource));
-});
-
-test("Release.latest", async ctx => {
-	await ctx.test("should exist", () => assert.ok(Release.latest.exists));
-});
-
-test("Release.tag", async ctx => {
-	await ctx.test("should not include the patch component if it's zero", () => assert.equal(existingRelease.tag, "1.0"));
-	await ctx.test("should include the patch component if it's greater than zero", () => assert.equal(nonExistentRelease.tag, "666.6.6"));
-});
-
-test("Release.url", async ctx => {
-	await ctx.test("should point to a GitHub tag if the release is provided as source code", () => {
-		assert.equal(nonExistentRelease.url.href, "https://github.com/HaxeFoundation/hashlink/archive/refs/tags/666.6.6.zip");
+	describe(".exists", () => {
+		it("should return `false` if the release does not exist", () => assert.ok(!nonExistentRelease.exists));
+		it("should return `true` if the release exists", () => assert.ok(existingRelease.exists));
 	});
 
-	await ctx.test("should point to a GitHub release if the release is provided as binary", () => {
-		assert.equal(existingRelease.url.href, "https://github.com/HaxeFoundation/hashlink/releases/download/1.0/hl-1.0.zip");
-	});
-});
-
-test("Release.find()", async ctx => {
-	await ctx.test("should return `undefined` if no release matches the version constraint", () => {
-		assert.ok(!Release.find("666.6.6"));
+	describe(".isSource", () => {
+		it("should return `false` if the release is provided as binary", () => assert.ok(!existingRelease.isSource));
+		it("should return `true` if the release is provided as source code", () => assert.ok(nonExistentRelease.isSource));
 	});
 
-	await ctx.test("should return the release corresponding to the version constraint if it exists", () => {
-		assert.equal(Release.find("*"), Release.latest);
-		assert.equal(Release.find("1.x"), Release.latest);
-		assert.equal(Release.find("=1.0.0")?.version, "1.0.0");
-		assert.equal(Release.find(">=1.0.0 <1.11.0")?.version, "1.10.0");
-	});
-});
-
-test("Release.get()", async ctx => {
-	await ctx.test("should return `undefined` if no release matches to the version number", () => {
-		assert.ok(!Release.get("666.6.6"));
+	describe(".latest", () => {
+		it("should exist", () => assert.ok(Release.latest.exists));
 	});
 
-	await ctx.test("should return the release corresponding to the version number if it exists", () => {
-		assert.equal(Release.get("1.0.0")?.version, "1.0.0");
-	});
-});
-
-test("Release.getAsset()", async ctx => {
-	await ctx.test("should return `undefined` if no asset matches the platform", () => {
-		assert.ok(!nonExistentRelease.getAsset(Platform.windows));
+	describe(".tag", () => {
+		it("should not include the patch component if it's zero", () => assert.equal(existingRelease.tag, "1.0"));
+		it("should include the patch component if it's greater than zero", () => assert.equal(nonExistentRelease.tag, "666.6.6"));
 	});
 
-	await ctx.test("should return the asset corresponding to the platform number if it exists", () => {
-		assert.equal(existingRelease.getAsset(Platform.windows)?.file, "hl-1.0.zip");
+	describe(".url", () => {
+		it("should point to a GitHub tag if the release is provided as source code", () => {
+			assert.equal(nonExistentRelease.url.href, "https://github.com/HaxeFoundation/hashlink/archive/refs/tags/666.6.6.zip");
+		});
+
+		it("should point to a GitHub release if the release is provided as binary", () => {
+			assert.equal(existingRelease.url.href, "https://github.com/HaxeFoundation/hashlink/releases/download/1.0/hl-1.0.zip");
+		});
+	});
+
+	describe(".find()", () => {
+		it("should return `undefined` if no release matches the version constraint", () => {
+			assert.ok(!Release.find("666.6.6"));
+		});
+
+		it("should return the release corresponding to the version constraint if it exists", () => {
+			assert.equal(Release.find("*"), Release.latest);
+			assert.equal(Release.find("1.x"), Release.latest);
+			assert.equal(Release.find("=1.0.0")?.version, "1.0.0");
+			assert.equal(Release.find(">=1.0.0 <1.11.0")?.version, "1.10.0");
+		});
+	});
+
+	describe(".get()", () => {
+		it("should return `undefined` if no release matches to the version number", () => {
+			assert.ok(!Release.get("666.6.6"));
+		});
+
+		it("should return the release corresponding to the version number if it exists", () => {
+			assert.equal(Release.get("1.0.0")?.version, "1.0.0");
+		});
+	});
+
+	describe(".getAsset()", () => {
+		it("should return `undefined` if no asset matches the platform", () => {
+			assert.ok(!nonExistentRelease.getAsset(Platform.windows));
+		});
+
+		it("should return the asset corresponding to the platform number if it exists", () => {
+			assert.equal(existingRelease.getAsset(Platform.windows)?.file, "hl-1.0.zip");
+		});
 	});
 });
--- test/setup_test.js
+++ test/setup_test.js
@@ -2,29 +2,34 @@ import assert from "node:assert/strict";
 import {existsSync} from "node:fs";
 import {join, resolve} from "node:path";
 import {env, platform} from "node:process";
-import test from "node:test";
+// @ts-expect-error TS2614
+import {describe, it} from "node:test";
 import {Platform, Release, Setup} from "../src/index.js";
 
-// Initializes the environment.
-if (!env.RUNNER_TEMP) env.RUNNER_TEMP = resolve("var/tmp");
-if (!env.RUNNER_TOOL_CACHE) env.RUNNER_TOOL_CACHE = resolve("var/cache");
+/**
+ * Tests the features of the {@link Setup} class.
+ */
+describe("Setup", () => {
+	if (!env.RUNNER_TEMP) env.RUNNER_TEMP = resolve("var/tmp");
+	if (!env.RUNNER_TOOL_CACHE) env.RUNNER_TOOL_CACHE = resolve("var/cache");
 
-test("Setup.download()", async ctx => {
-	await ctx.test("should properly download and extract the HashLink VM", async () => {
-		const setup = new Setup(Release.latest);
-		const output = await setup.download();
+	describe(".download()", () => {
+		it("should properly download and extract the HashLink VM", async () => {
+			const setup = new Setup(Release.latest);
+			const output = await setup.download();
 
-		let extension = setup.release.isSource ? ".vcxproj" : platform == Platform.windows ? ".exe" : "";
-		assert.ok(existsSync(join(output, `hl${extension}`)));
+			let extension = setup.release.isSource ? ".vcxproj" : platform == Platform.windows ? ".exe" : "";
+			assert.ok(existsSync(join(output, `hl${extension}`)));
 
-		extension = setup.release.isSource ? ".vcxproj" : platform == Platform.windows ? ".dll" : platform == Platform.macOS ? ".dylib" : ".so";
-		assert.ok(existsSync(join(output, `libhl${extension}`)));
+			extension = setup.release.isSource ? ".vcxproj" : platform == Platform.windows ? ".dll" : platform == Platform.macOS ? ".dylib" : ".so";
+			assert.ok(existsSync(join(output, `libhl${extension}`)));
+		});
 	});
-});
 
-test("Setup.install()", async ctx => {
-	await ctx.test("should add the HashLink VM binaries to the PATH environment variable", async () => {
-		const path = await new Setup(Release.latest).install();
-		assert.ok(env.PATH?.includes(path));
+	describe(".install()", () => {
+		it("should add the HashLink VM binaries to the PATH environment variable", async () => {
+			const path = await new Setup(Release.latest).install();
+			assert.ok(env.PATH?.includes(path));
+		});
 	});
 });
