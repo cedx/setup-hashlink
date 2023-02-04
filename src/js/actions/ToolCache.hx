139145fa87658dab0d1be9516d1d6837d9b89b3d
Cédric Belin <cedric@belin.io>
2023-02-04 19:22:49 +0100

    Ported the source code to Haxe 😁

---
+++ src/js/actions/ToolCache.hx
@@ -0,0 +1,32 @@
+package js.actions;
+
+import js.lib.Promise;
+
+/** Functions necessary for downloading and caching tools. **/
+@:jsRequire("@actions/tool-cache")
+extern class ToolCache {
+
+	/**
+		Caches a downloaded `directory` and installs it into the tool cache directory.
+		Returns the path of the location where the directory was cached.
+	**/
+	static function cacheDir(directory: String, tool: String, version: String, ?architecture: String): Promise<String>;
+
+	/**
+		Downloads a tool from the specified URL and streams it into a file.
+		Returns the path of the downloaded file.
+	**/
+	static function downloadTool(url: String, ?directory: String, ?authorization: String): Promise<String>;
+
+	/**
+		Extracts the specified ZIP archive into the given directory.
+		Returns the path of the location where the archive was extracted.
+	**/
+	static function extractZip(file: String, ?directory: String): Promise<String>;
+
+	/**
+		Finds the path to a tool version in the local installed tool cache.
+		Returns an empty string if the tool was not found.
+	**/
+	static function find(toolName: String, versionSpec: String, ?architecture: String): String;
+}
---
+++ src/setup_hashlink/Release.hx
@@ -0,0 +1,69 @@
+package setup_hashlink;
+
+import coconut.data.List;
+import coconut.data.Model;
+import haxe.Resource;
+import tink.Json;
+import tink.Url;
+import tink.semver.Constraint;
+import tink.semver.Version;
+
+/** Represents a GitHub release. **/
+@:jsonParse(json -> new setup_hashlink.Release(json))
+class Release implements Model {
+
+	/** The latest release. **/
+	public static var latest(get, never): Release;
+		static function get_latest() return data.first().sure();
+
+	/** The base URL of the releases. **/
+	static final baseUrl: Url = "https://github.com/HaxeFoundation/hashlink/";
+
+	/** The list of all releases. **/
+	static final data: List<Release> = (Json.parse(Resource.getString("releases.json")): Array<Release>);
+
+	/** The associated assets. **/
+	@:constant var assets: List<ReleaseAsset> = @byDefault new List();
+
+	/** Value indicating whether this release exists. **/
+	@:computed var exists: Bool = data.exists(release -> release.version == version);
+
+	/** Value indicating whether this release is provided as source code. **/
+	@:computed var isSource: Bool = getAsset(Sys.systemName()) == None;
+
+	/** The associated Git tag. **/
+	@:computed var tag: String = {
+		final semver: Version = version;
+		final tag = '${semver.major}.${semver.minor}';
+		semver.patch > 0 ? '$tag.${semver.patch}' : tag;
+	}
+
+	/** The download URL. **/
+	@:computed var url: Url = baseUrl.resolve(switch getAsset(Sys.systemName()) {
+		case None: 'archive/refs/tags/$tag.zip';
+		case Some(asset): 'releases/download/$tag/${asset.file}';
+	});
+
+	/** The version number. **/
+	@:constant var version: String = @byDefault "0.0.0";
+
+	/** Finds a release that matches the specified version `constraint`. **/
+	public static function find(constraint: Constraint) return data.first(release -> constraint.matches(release.version));
+
+	/** Gets the release corresponding to the specified `version`. **/
+	public static function get(version: Version) return data.first(release -> release.version == version);
+
+	/** Gets the asset corresponding to the specified `platform`. **/
+	public function getAsset(platform: Platform) return assets.first(asset -> asset.platform == platform);
+}
+
+/** Represents an asset of a GitHub release. **/
+@:jsonParse(json -> new setup_hashlink.Release.ReleaseAsset(json))
+class ReleaseAsset implements Model {
+
+	/** The target file. **/
+	@:constant var file: String;
+
+	/** The target platform. **/
+	@:constant var platform: Platform;
+}
