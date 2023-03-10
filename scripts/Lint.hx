ddc4ad66d4880b08eb6676e66672bb0beea31884
Cédric Belin <cedric@belin.io>
2023-03-10 15:03:38 +0100

    Updated the "Lint" task [skip ci]

--- scripts/Lint.hx
+++ scripts/Lint.hx
@@ -1,8 +1,3 @@
 /** Performs the static analysis of source code. **/
-function main() Sys.command("lix", ["run", "checkstyle",
-	"--config", "etc/checkstyle.json",
-	"--exitcode",
-	"--source", "scripts",
-	"--source", "src",
-	"--source", "test"
-]);
+function main()
+	Sys.command("lix run checkstyle --config etc/checkstyle.json --exitcode --source scripts --source src --source test");
