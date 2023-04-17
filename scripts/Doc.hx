a98b357907835ee3fec5e0a7cd0f1caf20d50af6
Cédric Belin <cedric@belin.io>
2023-04-17 11:07:12 +0200

    Update the "Doc" task [skip ci]

--- scripts/Doc.hx
+++ scripts/Doc.hx
@@ -11,7 +11,7 @@ function main() {
 	Sys.command("lix", ["run", "dox",
 		"--define", "description", "Set up your GitHub Actions workflow with a specific version of the HashLink VM.",
 		"--define", "source-path", "https://github.com/cedx/setup-hashlink/blob/main/src",
-		"--define", "themeColor", "0xffc105",
+		"--define", "themeColor", "0xea8220",
 		"--define", "version", Version.packageVersion,
 		"--define", "website", "https://github.com/cedx/setup-hashlink",
 		"--input-path", "var",
