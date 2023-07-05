13d6ac7f5e488f9dd2ca34a9ad59846791b77cb4
Cédric Belin <cedric@belin.io>
2023-07-05 18:46:11 +0200

    Update the "Build" task

--- scripts/Build.hx
+++ scripts/Build.hx
@@ -7,7 +7,4 @@ function main() {
 	final file = "bin/setup_hashlink.js";
 	Sys.command('npx ncc build $file --minify --out=var --target=es2022');
 	File.copy("var/index.js", file);
-
-	Sys.command('git update-index --chmod=+x $file');
-	if (Sys.systemName() != "Windows") Sys.command('chmod +x $file');
 }
