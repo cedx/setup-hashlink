4389427707ddc77d34e226687ba814f8c2e44dae
Cédric Belin <cedric@belin.io>
2023-07-21 13:37:49 +0200

    Replace `ncc` by `esbuild`

--- scripts/Build.hx
+++ scripts/Build.hx
@@ -2,9 +2,6 @@ import sys.io.File;
 
 /** Builds the project. **/
 function main() {
-	Sys.command("haxe build.hxml");
-
-	final file = "bin/setup_hashlink.js";
-	Sys.command('npx ncc build $file --minify --out=var --target=es2022');
-	File.copy("var/index.js", file);
+	final debug = Sys.args().contains("--debug");
+	Sys.command('haxe ${debug ? "--debug" : ""} build.hxml');
 }
--- scripts/Dist.hx
+++ scripts/Dist.hx
@@ -1,2 +1,6 @@
 /** Packages the project. **/
-function main() for (script in ["Clean", "Build", "Version"]) Sys.command('lix $script');
+function main() {
+	final file = "bin/setup_hashlink.js";
+	for (script in ["Clean", "Build", "Version"]) Sys.command('lix $script');
+	Sys.command('npx esbuild --allow-overwrite --bundle --legal-comments=none --log-level=warning --minify --outfile=$file --platform=node $file');
+}
