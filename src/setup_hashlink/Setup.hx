00d6d97e98c66038dd8d691dd65b3c4d10786f58
Cédric Belin <cedric@belin.io>
2023-08-26 19:19:38 +0200

    Code optimization

--- src/setup_hashlink/Setup.hx
+++ src/setup_hashlink/Setup.hx
@@ -95,9 +95,9 @@ class Setup {
 	function findSubfolder(directory: String) {
 		final folders = FileSystem.readDirectory(directory).filter(name -> FileSystem.isDirectory(Path.join([directory, name])));
 		return switch folders.length {
-			case 0: return Failure(new Error(NotFound, 'No subfolder found in: $directory.'));
-			case 1: return Success(folders.pop());
-			default: return Failure(new Error(Conflict, 'Multiple subfolders found in: $directory.'));
+			case 0: Failure(new Error(NotFound, 'No subfolder found in: $directory.'));
+			case 1: Success(folders.pop());
+			default: Failure(new Error(Conflict, 'Multiple subfolders found in: $directory.'));
 		}
 	}
 
