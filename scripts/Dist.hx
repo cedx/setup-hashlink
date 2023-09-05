efc274d8d59d29e5edd4cd74d929294e1a520cca
Cédric Belin <cedric@belin.io>
2023-09-05 10:36:51 +0200

    Update the "Dist" task

--- scripts/Dist.hx
+++ scripts/Dist.hx
@@ -3,4 +3,5 @@ function main() {
 	final file = "bin/setup_hashlink.js";
 	for (script in ["Clean", "Build", "Version"]) Sys.command('lix $script');
 	Sys.command('npx esbuild --allow-overwrite --bundle --legal-comments=none --log-level=warning --minify --outfile=$file --platform=node $file');
+	Sys.command('git update-index --chmod=+x $file');
 }
