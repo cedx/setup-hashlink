0184d399500c3f405177e2553a69196ddb7f6e33
Cédric Belin <cedric@belin.io>
2024-01-23 17:48:48 +0100

    Update the dependencies

---
+++ src/platform.ts
@@ -0,0 +1,20 @@
+/**
+ * Defines the operating system or platform of the HashLink VM.
+ */
+export enum OperatingSystem {
+
+	/**
+	 * Specifies a Linux platform.
+	 */
+	linux = "linux",
+
+	/**
+	 * Specifies a macOS platform.
+	 */
+	macOs = "darwin",
+
+	/**
+	 * Specifies a Windows platform.
+	 */
+	windows = "win32"
+}
