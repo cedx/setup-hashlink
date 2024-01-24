f3b22d16d31e6c7286f614ebccca66a6023a80fd
Cédric Belin <cedric@belin.io>
2024-01-24 15:37:19 +0100

    Port the action entry point

---
+++ src/cli.ts
@@ -0,0 +1,18 @@
+import {getInput, info, setFailed} from "@actions/core";
+import {Release, Setup} from "./index.js";
+
+/**
+ * Application entry point.
+ * @returns Resolves when Apache Ant has been installed.
+ */
+async function main(): Promise<void> {
+	const version = getInput("version");
+	const release = Release.find(!version || version == "latest" ? "*" : version);
+	if (!release) throw Error("No release matching the version constraint.");
+
+	const path = await new Setup(release).install();
+	info(`HashLink ${release.version} successfully installed in "${path}".`);
+}
+
+// Start the application.
+main().catch(error => setFailed(error instanceof Error ? error : String(error)));
