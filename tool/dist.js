7e01dbf982a6354a063a2b8b0a59a1ef9f591459
Cédric Belin <cedric@belin.io>
2024-01-26 18:23:36 +0100

    Update the build system

--- tool/dist.js
+++ tool/dist.js
@@ -1,6 +1,8 @@
-import {shebang} from "./tools.js";
+import {parseJson, replaceInFile, shebang} from "./tools.js";
 
 /**
  * Packages the project.
  */
+const {version} = await parseJson("../package.json");
+replaceInFile("README.md", /action\/v\d+(\.\d+){2}/, `action/v${version}`);
 shebang("bin/setup_hashlink.cjs");
--- tool/publish.js
+++ tool/publish.js
@@ -1,9 +1,8 @@
 import {execSync} from "node:child_process";
-import {parseJson, replaceInFile} from "./tools.js";
+import {parseJson} from "./tools.js";
 
 /**
  * Publishes the package.
  */
 const {version} = await parseJson("../package.json");
-replaceInFile("README.md", /action\/v\d+(\.\d+){2}/, `action/v${version}`);
 ["tag", "push origin"].forEach(action => execSync(`git ${action} v${version}`));
