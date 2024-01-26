d81bf93b46723bc3ba96ada62c53f16dbe79d6d7
Cédric Belin <cedric@belin.io>
2024-01-26 23:09:27 +0100

    Update the build system [skip ci]

--- tool/dist.js
+++ tool/dist.js
@@ -1,8 +1,10 @@
-import {parseJson, replaceInFile, shebang} from "./tools.js";
+import {readFileSync, writeFileSync} from "node:fs";
+import {EOL} from "node:os";
+import pkg from "../package.json" with {type: "json"};
 
-/**
- * Packages the project.
- */
-const {version} = await parseJson("../package.json");
-replaceInFile("README.md", /action\/v\d+(\.\d+){2}/, `action/v${version}`);
-shebang("bin/setup_hashlink.cjs");
+// Packages the project.
+const cli = "bin/setup_hashlink.cjs";
+writeFileSync(cli, `#!/usr/bin/env node${EOL}${readFileSync(cli, {encoding: "utf8"})}`);
+
+const readMe = "README.md";
+writeFileSync(readMe, readFileSync(readMe, {encoding: "utf8"}).replace(/action\/v\d+(\.\d+){2}/, `action/v${pkg.version}`));
