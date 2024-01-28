fc45f40b5903bf39ae7d9a62b4192b2c1482fa4e
Cédric Belin <cedric@belin.io>
2024-01-28 13:06:20 +0100

    Optimize the build task

--- tool/dist.js
+++ tool/dist.js
@@ -1,10 +1,6 @@
 import {readFileSync, writeFileSync} from "node:fs";
-import {EOL} from "node:os";
 import pkg from "../package.json" with {type: "json"};
 
 // Packages the project.
-const cli = "bin/setup_hashlink.cjs";
-writeFileSync(cli, `#!/usr/bin/env node${EOL}${readFileSync(cli, {encoding: "utf8"})}`);
-
-const readMe = "README.md";
-writeFileSync(readMe, readFileSync(readMe, {encoding: "utf8"}).replace(/action\/v\d+(\.\d+){2}/, `action/v${pkg.version}`));
+const file = "README.md";
+writeFileSync(file, readFileSync(file, {encoding: "utf8"}).replace(/action\/v\d+(\.\d+){2}/, `action/v${pkg.version}`));
