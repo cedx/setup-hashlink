c3b251379dc9a0ef68b8ff88ca8a68a91a9d5170
Cédric Belin <cedric@belin.io>
2024-04-27 19:44:05 +0200

    Update the build system

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,30 @@
 import {cp} from "node:fs/promises";
-import {env} from "node:process";
 import {deleteAsync} from "del";
-import esbuild from "esbuild";
 import {$} from "execa";
 import gulp from "gulp";
 import replace from "gulp-replace";
 import pkg from "./package.json" with {type: "json"};
-import buildOptions from "./etc/esbuild.js";
 
 // Builds the project.
-export async function build() {
-	await $`tsc --project src`;
-	return esbuild.build(buildOptions());
+export function build() {
+	return $`tsc --project src/tsconfig.json`;
 }
 
 // Deletes all generated files.
 export function clean() {
-	return deleteAsync(["bin/*.map", "lib", "var/**/*", "www"]);
-}
-
-// Packages the application.
-export async function dist() {
-	env.NODE_ENV = "production";
-	await build();
-	return $`git update-index --chmod=+x bin/setup_hashlink.cjs`;
+	return deleteAsync(["lib", "var/**/*", "www"]);
 }
 
 // Builds the documentation.
 export async function doc() {
 	for (const file of ["CHANGELOG.md", "LICENSE.md"]) await cp(file, `docs/${file.toLowerCase()}`);
-	await build();
 	return $`typedoc --options etc/typedoc.js`;
 }
 
 // Performs the static analysis of source code.
 export async function lint() {
-	await build();
-	await $`tsc --project .`;
-	return $`eslint --config=etc/eslint.config.js gulpfile.js etc src test`;
+	await $`tsc --project tsconfig.json`;
+	return $`eslint --config=etc/eslint.config.js gulpfile.js bin etc src test`;
 }
 
 // Publishes the package.
@@ -52,8 +39,7 @@ export async function serve() {
 }
 
 // Runs the test suite.
-export async function test() {
-	await build();
+export function test() {
 	return $`node --test --test-reporter=spec`;
 }
 
@@ -64,17 +50,9 @@ export function version() {
 		.pipe(gulp.dest("."));
 }
 
-// Watches for file changes.
-export async function watch() {
-	const context = await esbuild.context(buildOptions());
-	gulp.watch("src/**/*.ts", {ignoreInitial: false}, function buildCli() {
-		return context.rebuild();
-	});
-}
-
 // The default task.
 export default gulp.series(
 	clean,
-	dist,
+	build,
 	version
 );
