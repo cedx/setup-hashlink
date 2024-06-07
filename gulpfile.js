664b16eb740c095bdb2fcc6a08e09b4afb53d032
Cédric Belin <cedric@belin.io>
2024-06-07 15:59:01 +0200

    Exclude config files from static analysis [skip ci]

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ export async function doc() {
 // Performs the static analysis of source code.
 export async function lint() {
 	await $`tsc --project tsconfig.json`;
-	return $`eslint --config=etc/eslint.config.js gulpfile.js etc src test`;
+	return $`eslint --config=etc/eslint.config.js gulpfile.js src test`;
 }
 
 // Publishes the package.
