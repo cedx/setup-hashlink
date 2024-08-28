542ae138bb7a4d1274f0d84290de4ec99231b49a
Cédric Belin <cedric@belin.io>
2024-08-28 10:42:17 +0200

    Update the "Clean" script [skip ci]

--- scripts/Clean.hx
+++ scripts/Clean.hx
@@ -4,5 +4,6 @@ using Lambda;
 /** Deletes all generated files. **/
 function main() {
 	["js", "js.map"].map(ext -> 'bin/setup_hashlink.$ext').filter(FileSystem.exists).iter(FileSystem.deleteFile);
+	["lib", "res"].filter(FileSystem.exists).iter(Tools.removeDirectory);
 	Tools.cleanDirectory("var");
 }
