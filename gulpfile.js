1964dc100ba769b69ac7fffe682b1f6142848db5
Cédric Belin <cedx@outlook.com>
2025-09-18 11:52:40 +0200

    Enable the ESLint cache [skip ci]

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ export async function dist() {
 /** Performs the static analysis of source code. */
 export async function lint() {
 	await run("npx tsc --build tsconfig.json --noEmit");
-	await run("npx eslint --config=etc/ESLint.js gulpfile.js src test");
+	await run("npx eslint --cache --cache-location=var --config=etc/ESLint.js gulpfile.js src test");
 }
 
 /** Publishes the package. */
