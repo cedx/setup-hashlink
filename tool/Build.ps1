e224aa13d123de5c4b0722341837586869428709
Cédric Belin <cedx@outlook.com>
2025-10-06 19:53:34 +0200

    Update the "Build" script

--- tool/Build.ps1
+++ tool/Build.ps1
@@ -1,4 +1,4 @@
 Write-Host "Building the project..."
 npx tsc --build src/tsconfig.json
-npx esbuild "--banner:js=#!/usr/bin/env node" --bundle --legal-comments=none --log-level=warning --minify --outfile=bin/SetupHashLink.cjs --platform=node lib/Program.js
+npx esbuild lib/Program.js "--banner:js=#!/usr/bin/env node" --bundle --legal-comments=none --log-level=warning --minify --outfile=bin/SetupHashLink.cjs --platform=node
 git update-index --chmod=+x bin/SetupHashLink.cjs
