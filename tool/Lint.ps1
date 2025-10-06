7eb61848a2d7941cca173da313dd921f209ce039
Cédric Belin <cedx@outlook.com>
2025-10-06 10:09:40 +0200

    Update the "Lint" script [skip ci]

--- tool/Lint.ps1
+++ tool/Lint.ps1
@@ -1,3 +1,3 @@
 Write-Host "Performing the static analysis of source code..."
 npx tsc --build tsconfig.json --noEmit
-npx eslint --cache --cache-location=var --config=etc/ESLint.js example src test
+npx eslint --cache --cache-location=var --config=etc/ESLint.js src test
