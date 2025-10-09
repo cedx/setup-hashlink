f06be17df2c3fa65f88545504475e8bcfa53155a
Cédric Belin <cedx@outlook.com>
2025-10-09 02:44:36 +0200

    Update the "Lint" script [skip ci]

--- tool/Build.ps1
+++ tool/Build.ps1
@@ -1,4 +1,4 @@
-Write-Host "Building the project..."
+Write-Output "Building the project..."
 npx tsc --build src/tsconfig.json
 npx esbuild lib/Program.js "--banner:js=#!/usr/bin/env node" --bundle --legal-comments=none --log-level=warning --minify --outfile=bin/SetupHashLink.cjs --platform=node
 git update-index --chmod=+x bin/SetupHashLink.cjs
--- tool/Version.ps1
+++ tool/Version.ps1
@@ -1,3 +1,3 @@
-Write-Host "Updating the version number in the sources..."
+Write-Output "Updating the version number in the sources..."
 $version = (Get-Content "package.json" | ConvertFrom-Json).version
 (Get-Content "ReadMe.md") -replace "action\/v\d+(\.\d+){2}", "action/v$version" | Out-File "ReadMe.md"
