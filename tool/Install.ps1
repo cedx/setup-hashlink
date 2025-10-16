5566ba924292e38ae1bbb2379a27809012785cbb
Cédric Belin <cedx@outlook.com>
2025-10-17 01:17:08 +0200

    Update the CI

--- tool/Install.ps1
+++ tool/Install.ps1
@@ -1,5 +1,3 @@
 "Installing the dependencies..."
 $modules = Import-PowerShellDataFile Modules.psd1
-foreach ($module in $modules.Keys) {
-	if (-not (Get-InstalledPSResource $module)) { Install-PSResource $module }
-}
+foreach ($module in $modules.Keys) { Install-PSResource $module -WarningAction SilentlyContinue }
