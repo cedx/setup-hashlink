06683c3f7659b639f30d7bea1f2d2c3f7caaa66f
Cédric Belin <cedx@outlook.com>
2025-10-20 12:26:27 +0200

    Update the "Test" script

--- tool/Test.ps1
+++ tool/Test.ps1
@@ -2,12 +2,5 @@
 pwsh -Command {
 	Import-Module Pester
 	Invoke-Pester test
-
-	if (Get-Module BurntToast -ListAvailable) {
-		Import-Module BurntToast
-		$message = $LASTEXITCODE -eq 0 ? "were successfully completed" : "failed with $LASTEXITCODE $($LASTEXITCODE -gt 1 ? "errors" : "error")"
-		New-BurntToastNotification -AppLogo Program.ico -Text "Setup HashLink VM", "The tests $message." -UniqueIdentifier Belin.SetupHashLink
-	}
-
 	exit $LASTEXITCODE
 }
