b6a9f99cb33e882619dded49488493c34260e957
Cédric Belin <cedx@outlook.com>
2025-11-02 22:32:45 +0100

    Update the "Publish" script [skip ci]

--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -1,6 +1,6 @@
 . "$PSScriptRoot/Default.ps1"
 
-"Publishing the package..."
+"Publishing the module..."
 $version = (Import-PowerShellDataFile "SetupHashLink.psd1").ModuleVersion
 git tag "v$version"
 git push origin "v$version"
