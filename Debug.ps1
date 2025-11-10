5b137eaf440c72f85c5a0969d8058e922fe142a5
Cédric Belin <cedx@outlook.com>
2025-11-10 21:24:56 +0100

    Add a VSCode launch configuration [skip ci]

---
+++ Debug.ps1
@@ -0,0 +1,7 @@
+#!/usr/bin/env pwsh
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
+Set-StrictMode -Version Latest
+
+Import-Module "$PSScriptRoot/Cli.psd1"
+# Insert the command to be debugged here.
