29195d790c268684e3602460e16820a9cb9b199e
Cédric Belin <cedx@outlook.com>
2025-11-22 23:44:29 +0100

    Update the "Lint" script [skip ci]

--- tool/Lint.ps1
+++ tool/Lint.ps1
@@ -3,3 +3,4 @@ Import-Module PSScriptAnalyzer
 Invoke-ScriptAnalyzer $PSScriptRoot -Recurse
 Invoke-ScriptAnalyzer src -Recurse
 Invoke-ScriptAnalyzer test -Recurse
+Test-ModuleManifest "$PSScriptRoot/../SetupHashLink.psd1" | Out-Null
