f4399761d91efe2101d118c10372974b49b5ea1f
Cédric Belin <cedx@outlook.com>
2025-12-03 12:25:43 +0100

    Port the `Find-Release` cmdlet

--- src/Cmdlets/Find-Release.cs
+++ src/Cmdlets/Find-Release.cs
@@ -1,1 +1,20 @@
 namespace Belin.SetupHashLink.Cmdlets;
+
+/// <summary>
+/// Finds a release that matches the specified version constraint.
+/// </summary>
+[Cmdlet(VerbsCommon.Find, "Release")]
+[OutputType(typeof(Release))]
+public class FindReleaseCommand: Cmdlet {
+
+	/// <summary>
+	/// The version constraint.
+	/// </summary>
+	[Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true)]
+	public required string Constraint { get; set; }
+
+	/// <summary>
+	/// Performs execution of this command.
+	/// </summary>
+	protected override void ProcessRecord() => WriteObject(Release.Find(Constraint));
+}
