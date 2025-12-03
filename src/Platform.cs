8d959554282246790f4d505b61a84489dcbfec48
Cédric Belin <cedx@outlook.com>
2025-12-03 11:48:44 +0100

    Port the `Platform` enum and the `Get-Platform` cmdlet

--- src/Platform.cs
+++ src/Platform.cs
@@ -1,1 +1,22 @@
 namespace Belin.SetupHashLink;
+
+/// <summary>
+/// Identifies an operating system or platform.
+/// </summary>
+public enum Platform {
+
+	/// <summary>
+	/// Specifies a Linux platform.
+	/// </summary>
+	Linux,
+
+	/// <summary>
+	/// Specifies a macOS platform.
+	/// </summary>
+	MacOS,
+
+	/// <summary>
+	/// Specifies a Windows platform.
+	/// </summary>
+	Windows
+}
