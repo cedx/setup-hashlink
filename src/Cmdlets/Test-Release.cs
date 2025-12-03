ce81ec6806f3e3cfe50de4f1d6673030af6b3339
Cédric Belin <cedx@outlook.com>
2025-12-03 12:27:27 +0100

    Port the `Test-Release` cmdlet

--- src/Cmdlets/Test-Release.cs
+++ src/Cmdlets/Test-Release.cs
@@ -1,1 +1,29 @@
 namespace Belin.SetupHashLink.Cmdlets;
+
+/// <summary>
+/// Gets a value indicating whether a release with the specified version exists.
+/// </summary>
+[Cmdlet(VerbsDiagnostic.Test, "Release", DefaultParameterSetName = "Version")]
+[OutputType(typeof(bool))]
+public class TestReleaseCommand: PSCmdlet {
+
+	/// <summary>
+	/// The release to be tested.
+	/// </summary>
+	[Parameter(Mandatory = true, ParameterSetName = "InputObject", ValueFromPipeline = true)]
+	public required Release InputObject { get; set; }
+
+	/// <summary>
+	/// The version number of the release to be tested.
+	/// </summary>
+	[Parameter(Mandatory = true, ParameterSetName = "Version", Position = 0, ValueFromPipeline = true)]
+	public required Version Version { get; set; }
+
+	/// <summary>
+	/// Performs execution of this command.
+	/// </summary>
+	protected override void ProcessRecord() {
+		var release = ParameterSetName == "InputObject" ? InputObject : new Release(Version);
+		WriteObject(release.Exists);
+	}
+}
