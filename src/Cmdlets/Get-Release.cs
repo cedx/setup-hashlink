38c0c71be1ffc812a87909b08655c233b4bfc81b
Cédric Belin <cedx@outlook.com>
2025-12-03 12:26:21 +0100

    Port the `Get-Release` cmdlet

--- src/Cmdlets/Get-Release.cs
+++ src/Cmdlets/Get-Release.cs
@@ -1,1 +1,21 @@
 namespace Belin.SetupHashLink.Cmdlets;
+
+/// <summary>
+/// Gets the release corresponding to the specified version.
+/// </summary>
+[Cmdlet(VerbsCommon.Get, "Release")]
+[OutputType(typeof(Release))]
+public class GetReleaseCommand: Cmdlet {
+
+	/// <summary>
+	/// The version number. Use `*` or `Latest` to get the latest release.
+	/// </summary>
+	[Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true)]
+	public required string Version { get; set; }
+
+	/// <summary>
+	/// Performs execution of this command.
+	/// </summary>
+	protected override void ProcessRecord() =>
+		WriteObject(Release.LatestReleasePattern().IsMatch(Version) ? Release.Latest : Release.Get(Version));
+}
