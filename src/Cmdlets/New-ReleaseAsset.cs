ddbaacd137d327d59ec2608be715da970382d69e
Cédric Belin <cedx@outlook.com>
2025-12-03 11:54:47 +0100

    Port the `New-ReleaseAsset` cmdlet

--- src/Cmdlets/New-ReleaseAsset.cs
+++ src/Cmdlets/New-ReleaseAsset.cs
@@ -1,1 +1,26 @@
 namespace Belin.SetupHashLink.Cmdlets;
+
+/// <summary>
+/// Creates a new release asset.
+/// </summary>
+[Cmdlet(VerbsCommon.New, "ReleaseAsset")]
+[OutputType(typeof(Release.Asset))]
+public class NewReleaseAssetCommand: Cmdlet {
+	
+	/// <summary>
+	/// The target file.
+	/// </summary>
+	[Parameter(Mandatory = true, Position = 1)]
+	public required string File { get; set; }
+
+	/// <summary>
+	/// The target platform.
+	/// </summary>
+	[Parameter(Mandatory = true, Position = 0)]
+	public required Platform Platform { get; set; }
+
+	/// <summary>
+	/// Performs execution of this command.
+	/// </summary>
+	protected override void ProcessRecord() => WriteObject(new Release.Asset(Platform, File));
+}
