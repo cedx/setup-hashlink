d1b66546d22055e4f1441cc7ca6a94c8d9c14757
Cédric Belin <cedx@outlook.com>
2025-12-05 18:01:26 +0100

    Code optimization [skip ci]

--- test/Setup.Tests.cs
+++ test/Setup.Tests.cs
@@ -14,7 +14,7 @@ public sealed class SetupTests(TestContext testContext) {
 
 	[ClassInitialize]
 	public static void ClassInitialize(TestContext testContext) {
-		var baseDir = Path.Join(AppDomain.CurrentDomain.BaseDirectory, "../var");
+		var baseDir = Path.Join(AppContext.BaseDirectory, "../var");
 		if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("GITHUB_ENV"))) Environment.SetEnvironmentVariable("GITHUB_ENV", Path.Join(baseDir, "GitHub-Env.txt"));
 		if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("GITHUB_PATH"))) Environment.SetEnvironmentVariable("GITHUB_PATH", Path.Join(baseDir, "GitHub-Path.txt"));
 	}
