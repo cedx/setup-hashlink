50d7a33a0756146f39c38e94fabdb39cef654e1d
Cédric Belin <cedx@outlook.com>
2025-12-07 17:16:05 +0100

    Code optimization [skip ci]

--- src/Cmdlets/Install-Release.cs
+++ src/Cmdlets/Install-Release.cs
@@ -3,27 +3,27 @@ namespace Belin.SetupHashLink.Cmdlets;
 /// <summary>
 /// Installs the HashLink VM, after downloading it.
 /// </summary>
-[Cmdlet(VerbsLifecycle.Install, "Release", DefaultParameterSetName = "Constraint")]
+[Cmdlet(VerbsLifecycle.Install, "Release", DefaultParameterSetName = nameof(Constraint))]
 [OutputType(typeof(string))]
 public class InstallReleaseCommand: PSCmdlet {
 
 	/// <summary>
 	/// The version constraint of the release to be installed.
 	/// </summary>
-	[Parameter(Mandatory = true, ParameterSetName = "Constraint", Position = 0, ValueFromPipeline = true)]
+	[Parameter(Mandatory = true, ParameterSetName = nameof(Constraint), Position = 0, ValueFromPipeline = true)]
 	public required string Constraint { get; set; }
 
 	/// <summary>
 	/// The release to be installed.
 	/// </summary>
-	[Parameter(Mandatory = true, ParameterSetName = "InputObject", ValueFromPipeline = true)]
+	[Parameter(Mandatory = true, ParameterSetName = nameof(InputObject), ValueFromPipeline = true)]
 	public required Release InputObject { get; set; }
 
 	/// <summary>
 	/// Performs execution of this command.
 	/// </summary>
 	protected override void ProcessRecord() {
-		var release = ParameterSetName == "InputObject" ? InputObject : Release.Find(Constraint);
+		var release = ParameterSetName == nameof(InputObject) ? InputObject : Release.Find(Constraint);
 		if (release?.Exists ?? false) WriteObject(new Setup(release).Install().GetAwaiter().GetResult());
 		else {
 			var exception = new InvalidOperationException("No release matches the specified version constraint.");
--- src/Cmdlets/Test-Release.cs
+++ src/Cmdlets/Test-Release.cs
@@ -3,27 +3,27 @@ namespace Belin.SetupHashLink.Cmdlets;
 /// <summary>
 /// Gets a value indicating whether a release with the specified version exists.
 /// </summary>
-[Cmdlet(VerbsDiagnostic.Test, "Release", DefaultParameterSetName = "Version")]
+[Cmdlet(VerbsDiagnostic.Test, "Release", DefaultParameterSetName = nameof(Version))]
 [OutputType(typeof(bool))]
 public class TestReleaseCommand: PSCmdlet {
 
 	/// <summary>
 	/// The release to be tested.
 	/// </summary>
-	[Parameter(Mandatory = true, ParameterSetName = "InputObject", ValueFromPipeline = true)]
+	[Parameter(Mandatory = true, ParameterSetName = nameof(InputObject), ValueFromPipeline = true)]
 	public required Release InputObject { get; set; }
 
 	/// <summary>
 	/// The version number of the release to be tested.
 	/// </summary>
-	[Parameter(Mandatory = true, ParameterSetName = "Version", Position = 0, ValueFromPipeline = true)]
+	[Parameter(Mandatory = true, ParameterSetName = nameof(Version), Position = 0, ValueFromPipeline = true)]
 	public required Version Version { get; set; }
 
 	/// <summary>
 	/// Performs execution of this command.
 	/// </summary>
 	protected override void ProcessRecord() {
-		var release = ParameterSetName == "InputObject" ? InputObject : new Release(Version);
+		var release = ParameterSetName == nameof(InputObject) ? InputObject : new Release(Version);
 		WriteObject(release.Exists);
 	}
 }
