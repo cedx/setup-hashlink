e76025bff93ee728d4715ef538830d850f192b6c
Cédric Belin <cedx@outlook.com>
2025-12-16 11:52:24 +0100

    Code optimization

--- test/Cmdlets/Find-Release.Tests.ps1
+++ test/Cmdlets/Find-Release.Tests.ps1
@@ -22,7 +22,7 @@ Describe "Find-Release" {
 		(Find-HashLinkRelease "<=1.10")?.Version | Should -Be "1.10.0"
 	}
 
-	It "should throw if the version constraint is invalid" -ForEach @{ Version = "abc" }, @{ Version = "?1.10" } {
-		{ Find-HashLinkRelease $version } | Should -Throw
+	It "should throw if the version constraint is invalid" -ForEach "abc", "?1.10" {
+		{ Find-HashLinkRelease $_ } | Should -Throw
 	}
 }
