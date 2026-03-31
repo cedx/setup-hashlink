fac170a3cd11ea757432f276d3217ced187e64e6
Cédric Belin <cedx@outlook.com>
2026-03-31 19:54:01 +0200

    Port the release data

--- tool/Watch.ps1
+++
@@ -1,3 +0,0 @@
-"Watching for file changes..."
-$configuration = $Release ? "Release" : "Debug"
-Start-Process dotnet "watch build --configuration $configuration" -NoNewWindow -Wait -WorkingDirectory src
