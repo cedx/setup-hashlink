6b6d6a61cee1fadb1ccdc0a2ac2b8215c430decb
Cédric Belin <cedx@outlook.com>
2026-03-31 20:12:19 +0200

    Port the `Platform` enum

---
+++ src/Platform.psm1
@@ -0,0 +1,30 @@
+using namespace System.Diagnostics.CodeAnalysis
+
+<#
+.SYNOPSIS
+	Identifies an operating system or platform.
+#>
+enum Platform {
+	Linux
+	MacOS
+	Windows
+}
+
+<#
+.SYNOPSIS
+	Gets the current platform.
+.OUTPUTS
+	The current platform.
+#>
+function Get-Platform {
+	[CmdletBinding()]
+	[OutputType([Platform])]
+	[SuppressMessage("PSUseDeclaredVarsMoreThanAssignments", "")]
+	param ()
+
+	return $discard = switch ($true) {
+		{ $IsLinux } { [Platform]::Linux; break }
+		{ $IsMacOS } { [Platform]::MacOS; break }
+		default { [Platform]::Windows }
+	}
+}
