3df18bb2e1016bceec18ff1ef11d47e96e89eeef
Cédric Belin <cedx@outlook.com>
2026-04-02 18:13:21 +0200

    Fix the "Start" script

--- Start.ps1
+++ Start.ps1
@@ -6,7 +6,7 @@ Set-StrictMode -Version Latest
 Import-Module "$PSScriptRoot/SetupHashLink.psd1"
 if (-not (Test-Path Env:SETUP_HASHLINK_VERSION)) { $Env:SETUP_HASHLINK_VERSION = "Latest" }
 
-$release = Find-AntRelease $Env:SETUP_ANT_VERSION
+$release = Find-HashLinkRelease $Env:SETUP_ANT_VERSION
 if (-not $release) { throw "No release matches the specified version constraint." }
 
 $path = Install-HashLinkRelease -InputObject $release
