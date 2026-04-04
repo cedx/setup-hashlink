d6946624e043fddc86084fd32aef5003b64618d7
Cédric Belin <cedx@outlook.com>
2026-04-04 23:23:51 +0200

    Update the "Lint" script [skip ci]

--- tool/Lint.ps1
+++ tool/Lint.ps1
@@ -1,6 +1,4 @@
 "Performing the static analysis of source code..."
 Import-Module PSScriptAnalyzer
-Invoke-ScriptAnalyzer $PSScriptRoot -Recurse
-Invoke-ScriptAnalyzer src -Recurse
-Invoke-ScriptAnalyzer test -Recurse
+$PSScriptRoot, "src", "test" | Invoke-ScriptAnalyzer -Recurse
 Test-ModuleManifest SetupHashLink.psd1 | Out-Null
