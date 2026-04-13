03aa72ee4faac462db26db76e2493ba271f5be96
Cédric Belin <cedx@outlook.com>
2026-04-13 18:18:56 +0200

    Rename the tool helpers [skip ci]

--- tool/Outdated.ps1
+++ tool/Outdated.ps1
@@ -1,4 +1,4 @@
-using module ./PSResource.psm1
+using module ./Cmdlets.psm1
 
 "Checking for outdated dependencies..."
 (Import-PowerShellDataFile PSModules.psd1).Keys | Get-InstalledPSResource | Test-PSResourceUpdate
